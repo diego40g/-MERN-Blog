e90f723c300ecf4ff374007a4ada8cfeef2da2ed
diego40g <diegopaznaula@hotmail.com>
2022-11-20 23:04:44 -0500

    finished front end

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 
-const AddCommentForm = ({articleName, setArticleInfo}) => {
+const AddCommentForm = ({ articleName, setArticleInfo }) => {
   const [username, setUsername]=useState('');
   const [commentText, setCommentText]=useState('');
-  const addComments = async () => {
+
+  const addComment = async () => {
     const result = await fetch(`/api/articles/${articleName}/add-comments`, {
         method: "post",
         body: JSON.stringify({ username, text: commentText }),
@@ -20,14 +21,14 @@ const AddCommentForm = ({articleName, setArticleInfo}) => {
     <form className='shadow rounded px-8 pt-6 pb-8 mb-4'>
         <h3 className='text-xl fond-bold mb-4 text-gray-900'>
             Add a comment
-            </h3>
+        </h3>
         <label className='block text-gray-700 text-sm font-bold mb-2'>
             Name: 
         </label>
         <input 
             type='text' 
-            value={username}
-            onChange={(e)=>setUsername(e.target.value)}
+            value={ username }
+            onChange={(e) => setUsername(e.target.value)}
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-right focus:outline-none focus:shadow-outline'/>
         <label className='block text-gray-700 text-sm font-bold mb-2'>
             Comment:
@@ -36,9 +37,9 @@ const AddCommentForm = ({articleName, setArticleInfo}) => {
             rows='4' 
             cols='50'
             value={commentText}
-            onChange={(e)=>setCommentText(e.target.value)}
+            onChange={(e) => setCommentText(e.target.value)}
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-right focus:outline-none focus:shadow-outline'/>
-        <button onClick={()=>addComments()} className='bg-blue-500 hover:bg-blue-700 text-white font bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
+        <button onClick={() => addComment()} className='bg-blue-500 hover:bg-blue-700 text-white font bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
             Add comment
         </button>
     </form>
